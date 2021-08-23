ecadd34c757c97e5d6d648267ad7919146be1636
AnaMore21 <anastasiakrut1@gmail.com>
2021-08-24 01:18:56 +0300

    feat: implemetn 3 functions: max, min, and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        return Math.min(...array)};
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+    let sum = 0;
+    for (index in array) sum +=array[index];
+    return sum/array.length;
 }
+}
+
